1fbdc63da27c4a3cb124fe567fffa61f7b073390
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2024-03-06 18:14:02 -0800

    sjssjj

--- src/screens/Main/Home/HomeScreen.js
+++ src/screens/Main/Home/HomeScreen.js
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 import { COLORS, FONTS, SIZES, icons, images } from '../../../constants'
 import FormInput from '../../../components/Input/FormInput'
+import FormButton from '../../../components/Button/FormButton'
 
 const HomeScreen = () => {
     return (
@@ -22,8 +23,16 @@ const HomeScreen = () => {
             {/* BUTTON */}
             <View style={{ marginTop: SIZES.h1 * 1.2 }}>
                 <View style={styles.semesterCtn}>
-
+                    <TouchableOpacity style={{ flexDirection: 'row', alignItems: 'center' }}>
+                        <View style={styles.circle} />
+                        <Text style={{ ...FONTS.body4, color: COLORS.black, marginLeft: SIZES.base * 0.6 }}>1st Semester</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={{ flexDirection: 'row', alignItems: 'center' }}>
+                        <View style={[styles.circle, { borderWidth: 6 }]} />
+                        <Text style={{ ...FONTS.body4, color: COLORS.black, marginLeft: SIZES.base * 0.6 }}>2nd Semester</Text>
+                    </TouchableOpacity>
                 </View>
+                <FormButton title="CHECK RESULT" />
             </View>
         </View>
     )
@@ -43,6 +52,16 @@ const styles = StyleSheet.create({
         backgroundColor: COLORS.white,
         elevation: 5,
         borderRadius: SIZES.base,
-
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+        marginBottom: SIZES.h1
+    },
+    circle: {
+        height: SIZES.h2,
+        width: SIZES.h2,
+        borderRadius: 100,
+        borderWidth: 2,
+        borderColor: COLORS.primary,
     },
 })
