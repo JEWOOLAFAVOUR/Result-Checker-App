7f3fdc2f5c97e1df264fa609473f66e35123c901
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2024-03-06 17:29:59 -0800

    filed

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, StatusBar } from 'react-native'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import AuthStack from './src/navigator/AuthStack'
 import AppStack from './src/navigator/AppStack'
+import { COLORS } from './src/constants'
 
 const App = () => {
     const Stack = createNativeStackNavigator();
     return (
         <NavigationContainer>
+            <StatusBar barStyle={'dark-content'} backgroundColor={COLORS.white} />
             <Stack.Navigator screenOptions={{ headerShown: false }}>
                 <Stack.Screen name='Auth' component={AuthStack} />
                 <Stack.Screen name='Main' component={AppStack} />
--- src/screens/Auth/SplashScreen.js
+++ src/screens/Auth/SplashScreen.js
@@ -1,11 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
-import { COLORS, SIZES } from '../../constants'
+import { COLORS, SIZES, images } from '../../constants'
 
 const SplashScreen = () => {
     return (
         <View style={styles.page}>
-            <Text>SplashScreen</Text>
+            <Image source={images.logo} style={{ height: SIZES.h1 * 5, width: SIZES.h1 * 4 }} />
         </View>
     )
 }
@@ -16,6 +16,7 @@ const styles = StyleSheet.create({
     page: {
         flex: 1,
         backgroundColor: COLORS.white,
-
+        justifyContent: 'center',
+        alignItems: 'center',
     },
 })
